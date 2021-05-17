c4bbf8042c4a40ed5f7fcaf881d37c1867638d11
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-17 18:37:50 -0400

    add new task form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"; //use state to import a dynamic list of tasks
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,6 +26,13 @@ function App() {
     },
   ]);
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -42,6 +50,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
