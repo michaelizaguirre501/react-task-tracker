5faaf9c8fc98eb445565a5b3366d5862443773ac
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-17 17:49:12 -0400

    tasks come from app(state of tasks) and go to tasks component, then to task component. where it is passed to the task variable, the action onDelte goes from task->tasks->app

--- src/App.js
+++ src/App.js
@@ -24,10 +24,16 @@ function App() {
       reminder: true,
     },
   ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    console.log("delete", id);
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   ); // this is the ui page so we are bringing in our header and tasks components
 }
---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import { FaTimes } from "react-icons/fa";
+
+const Task = ({ task, onDelete }) => {
+  return (
+    <div className="task">
+      <h3>
+        {task.text}{" "}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)} //ACTIONS GO UP THE CHAIN, STATES GO DOWN, This ondelte is an action, you send the params up to app, state of things comes from app
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
