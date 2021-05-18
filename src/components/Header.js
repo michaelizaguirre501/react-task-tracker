93a7055b0e2b48d83100f25aef25e2c0e4487860
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-17 23:16:33 -0400

    add task form is toggle, button and text changes depending on the show add state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import PropTypes from "prop-types"; //formatter
 import Button from "./Button"; // import button from button component
 
-const Header = ({ title }) => {
-  // header with title prop
-  const onClick = () => {
-    console.log("click");
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
+  // header with title prop and onAdd func as a prop
   return (
     // using title prop inside the h1
     // the button here is settingt he color text and defining onclick to run the onClick func above, now you can make many buttons with different values for those 'attributes' called props
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAdd ? "#d0021b" : "#417505"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
