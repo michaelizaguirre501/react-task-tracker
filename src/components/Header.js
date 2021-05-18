03f28d79e713aa6ee3c2d8d7ab36e70ee9f414e1
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-18 00:37:08 -0400

    button gone on about

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types"; //formatter
 import Button from "./Button"; // import button from button component
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   // header with title prop and onAdd func as a prop
   return (
     // using title prop inside the h1
     // the button here is settingt he color text and defining onclick to run the onClick func above, now you can make many buttons with different values for those 'attributes' called props
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "#d0021b" : "#417505"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" ? (
+        <Button
+          color={showAdd ? "#d0021b" : "#417505"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      ) : (
+        ""
+      )}
     </header>
   );
 };
